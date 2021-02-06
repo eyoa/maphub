c53bd4b76b8de6ffbe7221e822f72810cf28c316
Evelyn Yoa <yoaevelyn@gmail.com>
2021-02-06 22:18:07 +0000

    add schema for tables users and maps

--- server/db/schema/01_users.sql
+++ server/db/schema/01_users.sql
@@ -1,7 +1,21 @@
--- Drop and recreate Users table (Example)
-
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS maps CASCADE;
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  username VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  description TEXT,
+  profile_img_url VARCHAR(255) DEFAULT "/images/profile-hex.png"
+);
+
+CREATE TABLE maps (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  latitude DECIMAL(8,6) NOT NULL,
+  longitudes DECIMAL(9,6) NOT NULL,
+  title VARCHAR(255),
+  zoom_lv INTEGER,
+  description TEXT
 );
