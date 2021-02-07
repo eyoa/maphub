3a5cd7dd3e1bef61d1488e92afc301feac93087e
Sang Hyup Lee <tms.lee9@gmail.com>
2021-02-07 01:33:36 +0000

    wrote userRoutes

--- server/routes/users.js
+++ server/routes/users.js
@@ -8,31 +8,85 @@
 const express = require('express');
 const router  = express.Router();
 
+//NOTE: need method override for put
+
 module.exports = (db) => {
-  router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users;`)
-      .then(data => {
-        const users = data.rows;
-        res.json({ users });
+
+  //login function
+  const login = function(email, password) {
+    /*
+      return db.getUser(email)
+      .then(user => {
+        if (bcrypt.compareSync(password, user.password)) {
+          return user;
+        }
+        return null;
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  });
+    */
+  }
 
+  //get user data
   router.get("/users/:id", (req, res) => {
-
+    /*
+    return db.getUser(userId)
+    .then(user => user);
+    .catch(err => ....)
+    db query SELECT * FORM users where id = id
+    */
   });
 
-  router.put()
+  //update user data
+  router.put("/users/:id", (req, res) => {
+    /*
+    db.setUser(username, password, email ....)
+    .then(updatedUser => updatedUser)
+    .catch(err => ...)
+      db query UPDATE users SET... WHERE id=id
+    */
+  });
+
+  //login
+  router.put("/login", (req, res) => {
+    /*
+    const {email, password} = req.body;
+    login(email, password)
+    .then(user => {
+      if (!user) {
+        res. send({error: "error"});
+        return;
+      }
+      req.session.userId = user.id;
+      res.send(....);
+    })
+    .catch(e => res.send(e));
+    */
+  });
+
+  //logout
+  router.put("/logout", (req, res) => {
+    /*
+    req.session.userId = null;
+    res.send(....) redirect to main page
+    */
+  });
 
 
-
-
-
-
+  //create new user
+  router.put("/register", (req, res) => {
+    /*
+    const user = req.body;
+    user.password = bcrypt.hashSync(user.password, 12);
+    db.addUser(user)
+    .then(user => {
+      if (!user) {
+        res.send({error: "error"});
+        return;
+      }
+      req.session.userId = user.id;
+    })
+    .catch(e => res.send(e));
+    */
+  });
 
   return router;
 };
