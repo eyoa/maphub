a2cbf6a81b02a0e31ca88481b263f346a3133279
Evelyn Yoa <yoaevelyn@gmail.com>
2021-02-08 16:14:48 +0000

    add tmp links to views for dev testing in navbar

--- public/scripts/components/navbar.js
+++ public/scripts/components/navbar.js
@@ -7,9 +7,35 @@ $(() => {
     <i class="fas fa-map"></i>
     MapHub
     </a>
+  <ul class="nav justify-content-center">
+    <li class="nav-item">
+      <a class="nav-link active" href="#">Browse</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Map</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Profile</a>
+    </li>
+  </ul>
     <ul class="navbar-nav ml-auto">
-      <button type="button" class="btn btn-info">Login</button>
+      <a class="btn btn-primary" href="/users/login" role="button">Login</a>
     </ul>
+
+
+    <div class="btn-group">
+  <button type="button" class="btn btn-info"> :)</button>
+  <button type="button" class="btn btn-info dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    <span class="sr-only">Toggle Dropdown</span>
+  </button>
+  <div class="dropdown-menu">
+    <a class="dropdown-item" href="#">Action</a>
+    <a class="dropdown-item" href="#">Another action</a>
+    <a class="dropdown-item" href="#">Something else here</a>
+    <div class="dropdown-divider"></div>
+    <a class="dropdown-item" href="#">Separated link</a>
+  </div>
+</div>
 
   </nav>
 
