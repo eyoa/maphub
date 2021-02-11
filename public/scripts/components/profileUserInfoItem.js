9275526cb2dadb0a30cb0bb4a0c44a9efa835d77
Evelyn Yoa <yoaevelyn@gmail.com>
2021-02-11 22:13:11 +0000

    add styling to profile edit form

--- public/scripts/components/profileUserInfoItem.js
+++ public/scripts/components/profileUserInfoItem.js
@@ -34,35 +34,40 @@ $(() => {
     return `
       <div class="container" id="edit-profile-item">
         <form id="edit-user-profile">
-          <div class="row">
+          <div class="p-2 d-flex flex-row justify-content-center">
 
-            <div class="col">
+            <div>
               <img id="user-profile-img" src="${user.profile_img_url}">
-
-              <label for="edit-profile-img">new profile image url: </label>
-              <input type="url" class="form-control" id="edit-profile-img" value="${user.profile_img_url}">
-              <button class='btn btn-primary' id="refresh-profile-image">preview profile image</button>
             </div>
 
-            <div class="col">
-              <label for="edit-username">new username: </label>
-              <input type="text" class="form-control" id="edit-username" value="${user.username}">
+            <div class="p-2 d-flex flex-column justify-content-center">
+              <div class="form-group">
+                <label for="edit-username">new username: </label>
+                <input type="text" class="form-control" id="edit-username" value="${user.username}">
+              </div>
 
-              <label for="edit-email">new email: </label>
-              <input type="email" class="form-control" id="edit-email" value="${user.email}">
+              <div class="form-group my-2">
+                <label for="edit-email">new email: </label>
+                <input type="email" class="form-control" id="edit-email" value="${user.email}">
+              </div>
+              <div class="form-group my-2">
+                <label for="edit-description">new description: </label>
+                <input type="text" class="form-control" id="edit-description" value="${user.description}">
+              </div>
+
+              <div class="form-group my-2">
+                <label for="edit-profile-img">new profile image url: </label>
+                <input type="url" class="form-control" id="edit-profile-img" value="${user.profile_img_url}">
+                <button class='btn btn-primary my-3 mx-auto' id="refresh-profile-image">preview profile image</button>
+              </div>
+
+              <div class="my-2">
+                <button type="submit" class="btn btn-primary mx-auto" id="save-profile-edit-changes">save changes</button>
+                <button class="btn btn-primary mx-auto " id="cancel-profile-edit-changes">discard changes</button>
             </div>
-
           </div>
-              <label for="edit-description">new description: </label>
-              <input type="text" class="form-control" id="edit-description" value="${user.description}">
 
-          <div class="row">
-            <div class="col">
-              <button type="submit" class="btn btn-primary" id="save-profile-edit-changes">save changes</button>
-            </div>
-            <div class="col">
-              <button class="btn btn-primary" id="cancel-profile-edit-changes">discard changes</button>
-            </div>
+
           </div>
         </form>
       </div>
